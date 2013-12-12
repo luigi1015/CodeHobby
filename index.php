440ac7498e637c3d5d77210aff10cd87683487de
luigi1015 <github@314ghz.com>
2013-12-11 22:38:17 -0600

    Move the social buttons and made some minor cosmetic changes.

--- index.php
+++ index.php
@@ -15,23 +15,33 @@
 		<link rel="stylesheet" media="screen" href="codehobby.css" />
 	</head>
 	<body>
-
 	<!--<div id="header">-->
 		<?php include 'header.html'; ?>
 	<!--</div>-->
 	<div id="main"><!--Main Section-->
-	<div id="content-main">
-		<?php include 'main.html'; ?>
-	</div>
-	<div id="content-projects">
-		<?php include 'projects.html'; ?>
-	</div>
-	<div id="content-forum">
-		<?php include 'forum.html'; ?>
-	</div>
-	<div id="content-contact">
-		<?php include 'contact.html'; ?>
-	</div>
+		<table>
+			<tr>
+				<td id="table-main">
+		<div id="content-main">
+			<?php include 'main.html'; ?>
+		</div>
+		<div id="content-projects">
+			<?php include 'projects.html'; ?>
+		</div>
+		<div id="content-forum">
+			<?php include 'forum.html'; ?>
+		</div>
+		<div id="content-contact">
+			<?php include 'contact.html'; ?>
+		</div>
+				</td>
+				<td id="table-social">
+		<div>
+			<?php include 'social.html'; ?>
+		</div>
+				</td>
+			</tr>
+		</table>
 	</div><!--Main Section-->
 	<div id="footer">
 		<?php include 'footer.html'; ?>
